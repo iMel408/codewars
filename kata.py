29214bca56dda44a4d8155629793e71a03c5ca75
Melissa <melissascampini@gmail.com>
2019-02-24 16:39:34 -0800

    first commit

--- kata.py
+++ kata.py
@@ -1,9 +1,38 @@
 from datetime import date
 
-def unlucky_days(year):
+#### unlucky_days - for loop vs. List comprehension ####
 
-    return sum(date(year, month, 13).isoweekday() == 5 for month in range(1, 13))
+def unlucky_days_a(year):
+    ''' for loop'''
+    count = 0
+    for month in range(1,13):
+        if date(year,month,13).isoweekday() == 5:
+            count += 1
+    return count
 
-print(unlucky_days(2018))
-print(unlucky_days(1978))
-print(unlucky_days(2007))
+def unlucky_days_b(year):
+    ''' list comprehension '''
+
+    return sum(date(year,month,13).isoweekday() == 5 for month in range(1,13))
+
+print(unlucky_days_a(2019))
+print(unlucky_days_b(2019))
+
+
+
+
+
+
+
+
+
+
+
+
+def bday_of_week():
+
+    return (date(y))
+
+
+
+
